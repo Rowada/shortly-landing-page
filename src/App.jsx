ed8f3428c6819a74da0957cf753da168258d63aa
Josue <96113041+Rowada@users.noreply.github.com>
2024-04-16 11:57:49 +0200

    style: add custom classes for colors and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="bg-red-300 p-8 text-5xl text-red-50">Hello World</h1>
+      <h1 className="bg-red-300 p-8 text-5xl font-extralight text-red-50">
+        Hello World
+      </h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Poppins", "sans-serif"],
+      },
+
+      colors: {
+        primary: "hsl(180, 66%, 49%)",
+        background: "hsl(257, 27%, 26%)",
+        secondary: "hsl(0, 87%, 67%)",
+        body: "hsl(0, 0%, 75%)",
+        accent: "hsl(257, 7%, 63%)",
+        text: "hsl(255, 11%, 22%)",
+        foreground: "hsl(260, 8%, 14%)",
+      },
+    },
   },
   plugins: [],
 };
