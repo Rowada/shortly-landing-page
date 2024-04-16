c2d0a7598104b75a1f8efb7c501d7c443f8f1686
Josue <96113041+Rowada@users.noreply.github.com>
2024-04-16 11:34:34 +0200

    build: add eslint config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="p-8 bg-red-300 text-5xl text-red-50">Hello World</h1>
+      <h1 className="bg-red-300 p-8 text-5xl text-red-50">Hello World</h1>
     </>
   );
 }
