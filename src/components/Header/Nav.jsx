91e92b0264df8c2432502c49247937a777fe31c0
Josue <96113041+Rowada@users.noreply.github.com>
2024-04-26 10:06:06 +0200

    style: update mobile navbar

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -62,8 +62,8 @@ export const Nav = () => {
         </div>
 
         {isOpen && (
-          <div className=" md:hidden">
-            <div className=" mx-auto flex flex-col items-center justify-around space-y-4 rounded-lg bg-background py-4">
+          <div className="absolute inset-x-0 z-50 m-auto w-[90%] md:hidden">
+            <div className="mx-auto flex w-full flex-col items-center justify-around space-y-4 rounded-lg bg-background py-4">
               <ul className="flex flex-col items-center space-y-4">
                 {MENU_ITEMS.map((items) => (
                   <li key={items.id}>
