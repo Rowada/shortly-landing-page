1a9b4cbca538eeda3beda6e2ebae3b9e98a9aa9e
Josue <96113041+Rowada@users.noreply.github.com>
2024-05-31 10:05:23 +0200

    style: add mobile view for UrlCard

--- src/components/Form/UrlCard.jsx
+++ src/components/Form/UrlCard.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 export const UrlCard = ({ longUrl, shortUrl }) => {
   return (
-    <div className=" mt-4 flex items-center justify-between rounded-md bg-white p-4">
-      {longUrl && <p>{longUrl}</p>}
+    <div className="mt-4 flex w-full flex-col items-center gap-2 rounded-md bg-white p-4 md:flex-row md:justify-between">
+      {longUrl && (
+        <p className="w-full border-b border-primary p-1 text-center md:w-auto md:border-none">
+          {longUrl}
+        </p>
+      )}
 
-      <div className="flex items-center justify-center gap-3">
+      <div className="flex w-full flex-col items-center gap-2 md:w-auto md:flex-row md:justify-center md:gap-3">
         {shortUrl && <span className="text-primary">{shortUrl}</span>}
 
-        <button className="w-full rounded-md bg-primary px-4 py-2 font-semibold text-white">
+        <button className="w-full rounded-md bg-primary px-4 py-2 font-semibold text-white md:w-auto">
           Copy
         </button>
       </div>
