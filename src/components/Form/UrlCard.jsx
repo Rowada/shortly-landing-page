08e99deb6250645a050816082586ce6580c13232
Josue <96113041+Rowada@users.noreply.github.com>
2024-06-10 09:40:31 +0200

    feat: add copy url function

--- src/components/Form/UrlCard.jsx
+++ src/components/Form/UrlCard.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 export const UrlCard = ({ longUrl, shortUrl }) => {
+  const copyToClipborad = () => {
+    navigator.clipboard.writeText(shortUrl);
+  };
+
   return (
     <div className="mt-4 flex w-full flex-col items-center gap-2 rounded-md bg-white p-4 md:flex-row md:justify-between">
       {longUrl && (
@@ -12,7 +16,10 @@ export const UrlCard = ({ longUrl, shortUrl }) => {
       <div className="flex w-full flex-col items-center gap-2 md:w-auto md:flex-row md:justify-center md:gap-3">
         {shortUrl && <span className="text-primary">{shortUrl}</span>}
 
-        <button className="w-full rounded-md bg-primary px-4 py-2 font-semibold text-white md:w-auto">
+        <button
+          onClick={copyToClipborad}
+          className="w-full rounded-md bg-primary px-4 py-2 font-semibold text-white md:w-auto"
+        >
           Copy
         </button>
       </div>
