20971ee19dd1ac972a361b59b0c2f71a0e2534ad
Josue <96113041+Rowada@users.noreply.github.com>
2024-06-24 10:52:15 +0200

    feat: update mobile view and add loader icon

--- src/components/Form/CleanForm.jsx
+++ src/components/Form/CleanForm.jsx
@@ -1,7 +1,8 @@
 // @ts-nocheck
 
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import useCleanURI from "../../hooks/useCleanURI";
+import { LoaderCircle } from "lucide-react";
 
 export const CleanForm = () => {
   const [localLongUrl, setLocalLongUrl] = useState("");
@@ -36,19 +37,12 @@ export const CleanForm = () => {
       <div className="flex h-[150px] md:h-32 w-full items-center justify-center rounded-md bg-background bg-[url('src/assets/images/bg-shorten-desktop.svg')] bg-cover bg-center bg-no-repeat text-center ">
         <form
           onSubmit={handleShortenUrl}
-          className="m-auto flex size-full flex-col items-center justify-center gap-y-3 px-3 md:flex-row md:gap-x-3 md:px-10"
+          className="m-auto flex size-full flex-col items-center justify-evenly md:justify-center gap-y-3 px-3 md:flex-row md:gap-x-3 md:px-10"
         >
-          {/* <input
-            className="w-full rounded-md p-3 md:w-4/5 focus:outline-none focus:border-secondary focus:ring-2 focus:ring-secondary"
-            type="text"
-            placeholder="Shorten a link here..."
-            value={localLongUrl}
-            onChange={(e) => setLocalLongUrl(e.target.value)}
-          /> */}
           <div className="relative w-full md:w-4/5">
             <input
               className={`w-full rounded-md p-3 focus:outline-none focus:border-secondary focus:ring-2 focus:ring-secondary ${
-                inputError ? "mt-3 md:mt-0" : ""
+                inputError ? "mt-3 md:mt-0 " : ""
               }`}
               type="text"
               placeholder="Shorten a link here..."
@@ -56,15 +50,15 @@ export const CleanForm = () => {
               onChange={(e) => setLocalLongUrl(e.target.value)}
             />
             {inputError && (
-              <p className="absolute left-0 text-red-500 text-sm mt-1 md:mt-0 md:bottom-auto">
+              <p className="absolute left-0 text-red-500 text-xs md:text-sm mt-1 md:bottom-auto">
                 {inputError}
               </p>
             )}
           </div>
           <button
             type="submit"
-            className={`w-full rounded-md bg-primary p-3 font-semibold text-white md:w-1/5 ${
-              inputError ? "mt-6 mb-3 md:mt-0 md:mb-0" : ""
+            className={`w-full focus:outline-none focus:border-accent focus:ring-2 focus:ring-accent rounded-md bg-primary p-3 font-semibold text-white md:w-1/5 ${
+              inputError ? "mt-4 mb-3 md:mt-0 md:mb-0" : ""
             }`}
           >
             Shorten it!
@@ -79,9 +73,9 @@ export const CleanForm = () => {
         {error && <p className="rounded-md bg-secondary p-3">{error}</p>}
 
         {loading && (
-          <p className="rounded-md bg-primary p-3 font-semibold text-white">
-            Loading...
-          </p>
+          <div className="flex items-center justify-center p-3">
+            <LoaderCircle size={52} className="text-primary animate-spin" />
+          </div>
         )}
       </div>
     </>
